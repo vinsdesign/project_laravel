10403d0545bf253f4aa0bc07e9ed327b9f280e3e
evinsdesign <kelvinsaputra024@gmail.com>
2024-10-10 15:53:48 +0800

    feat: Model Teams

---
+++ app/Models/team.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class team
+{
+    public static function all(){
+        return [
+            [
+                'name' => 'Kelvin Saputra',
+                'division' => 'Project Manager',
+                'Moto'=>'',
+                'image' => 'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            ],
+            [
+                'name' => 'Arika Subawa',
+                'division' => 'Programmer Specialist',
+                'image' => 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXZhdGFyfGVufDB8fDB8fHww',
+            ],
+            [
+                'name' => 'Lionel Dode',
+                'division' => 'Data Analytic',
+                'image' => 'https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            ],
+            [
+                'name' => 'Cristiano Agus',
+                'division' => 'Asisten Manager',
+                'image' => 'https://plus.unsplash.com/premium_photo-1664536392779-049ba8fde933?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            ],
+        ];
+    }
+    
+}
---
+++ resources/views/team-detail.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+<div class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+    <div>
+        <div class="flex items-center gap-x-6">
+            <img class="h-16 w-16 rounded-full  object-cover"
+                src="{{ $team['image'] }}"
+                alt="">
+            <div>
+                <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">{{ $team['name'] }}
+                </h3>
+                <p class="text-sm font-semibold leading-6 text-indigo-600">{{ $team['division'] }}</p>
+            </div>
+        </div>
+    </div>
+</div>
+</x-layout>
