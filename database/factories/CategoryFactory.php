d31cb6e7749ed76ea7d600dc294ec727c563963f
evinsdesign <kelvinsaputra024@gmail.com>
2024-10-25 11:04:17 +0800

    feat: trying DatabaseSeeder

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -19,7 +19,7 @@ class CategoryFactory extends Factory
     {
         return [
             'name' => fake()->sentence(rand(1,2), false),
-            'slug'=> Str::slug(fake()->sentence(rand(1,2), false)),
+            'slug'=> Str::slug(fake()->sentence()),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\article;
+use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $admin = User::create([
+            'name'  => 'Kylian Saputra',
+            'username' => 'KylianSaputra',
+            'email' => 'kyliansaputra@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10)
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
         ]);
-    }
+
+        article::factory(50)->recycle(
+            user::factory(10)->create(),
+            $admin,   
+            Category::factory(5)->create(),
+        )->create();
+     }
 }
