de907361e9a3aeea3d6ff2132f09fc285fc85598
vinsdesign <kelvinsaputra024@gmail.com>
2024-11-02 14:03:13 +0800

    feat: config tailwind with plugin flowbite

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,13 +2,12 @@
     <div class="bg-white py-24 sm:py-32">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
           <div class="mx-auto max-w-2xl lg:mx-0">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ $title }}</h2>
-            <p class="mt-2 text-lg leading-8 text-gray-600">Learn how to grow your business with our expert advice.</p>
+            <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{{ $title }}</h2>
+            <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
           </div>
           {{-- Team --}}
          <!-- resources/views/blog.blade.php -->
           <x-article :articles="$articles"></x-article>
-          </div>
         </div>
     </div>
 </x-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,21 @@ export default {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
-  ],  theme: {
+    "./node_modules/flowbite/**/*.js"
+  ],  
+  darkMode: 'class',
+  theme: {
     extend: {
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+      },
       fontFamily:{
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
